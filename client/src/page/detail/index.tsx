bd099bf73a12549dcc3b92db0e76158dc2d20808
zcs02468 <zcs02468@163.com>
2020-05-18 21:02:33 +0800

    添加详情页页面 tsx

--- client/src/page/detail/index.tsx
+++ client/src/page/detail/index.tsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-
+import { ClockCircleOutlined, EyeOutlined, CommentOutlined } from "@ant-design/icons";
+import "./style.scss";
 
 class DetailPage extends Component {
     render() {
         return (
-            <div>详情页</div>
-        )
+            <div className="detail-page">
+                <div className="content-wrap">
+                    <div className="post">
+                        <header className="post-header">
+                            <h1>如何防止PHP进行SQL注入？</h1>
+                            <div className="post-meta">
+                                <span className="time">
+                                    <ClockCircleOutlined /> 发表于 <time>2020-05-12</time>
+                                </span>
+                                <span className="look">
+                                    <EyeOutlined /> 浏览量 411
+                                </span>
+                                <span className="interactionCount">
+                                    <a className="discussionUrl" href="https://www.luymm.com/archives/402/#comments">
+                                        <CommentOutlined /> 没有评论
+                                    </a>
+                                </span>
+                            </div>
+                        </header>
+                        <div className="post-content"></div>
+                        <header className="post-footer"></header>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default DetailPage
+export default DetailPage;
