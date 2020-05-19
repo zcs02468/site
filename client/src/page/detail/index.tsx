7c39cae0565fabe460f691dd3d74ada3abd4fdfb
zcs02468 <zcs02468@163.com>
2020-05-19 20:30:08 +0800

    1.添加详情页 2,添加彩带背景

--- client/src/page/detail/index.tsx
+++ client/src/page/detail/index.tsx
@@ -1,18 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 import { ClockCircleOutlined, EyeOutlined, CommentOutlined } from "@ant-design/icons";
+import Prism from "prismjs";
+import moment from 'moment';
+// import 'moment/locale/en-ca';
+import { getBlogContent } from "../../axios/index";
 import "./style.scss";
 
-class DetailPage extends Component {
+interface Props {
+    match: {
+        isExact: boolean;
+        params: any;
+        path: string;
+        url: string;
+    }
+}
+
+class DetailPage extends React.PureComponent<Props, {}> {
+    state = {
+        outputContent: "",
+        title: '',
+        createTime: ""
+    };
+
+    async componentDidMount() {
+        let [res] = await getBlogContent(this.props.match.params.id)
+        
+        this.setState({
+            outputContent: res.data.htmlContent,
+            title: res.data.title,
+            createTime: moment(Number(res.data.createTime)).format(`YYYY-MM-DD`)
+        });
+        Prism.hooks.add('before-highlight', function(env: any) {
+            env.element.innerHTML = env.element.innerHTML.replace(/<br\s*\/?>/g,'\n');
+            env.code = env.element.textContent.replace(/^(?:\r?\n|\r)/,'');
+        });
+    }
+
     render() {
         return (
             <div className="detail-page">
                 <div className="content-wrap">
                     <div className="post">
                         <header className="post-header">
-                            <h1>如何防止PHP进行SQL注入？</h1>
+                            <h1>{ this.state.title }</h1>
                             <div className="post-meta">
                                 <span className="time">
-                                    <ClockCircleOutlined /> 发表于 <time>2020-05-12</time>
+                                    <ClockCircleOutlined /> 发表于 <time>{this.state.createTime}</time>
                                 </span>
                                 <span className="look">
                                     <EyeOutlined /> 浏览量 411
@@ -24,7 +57,9 @@ class DetailPage extends Component {
                                 </span>
                             </div>
                         </header>
-                        <div className="post-content"></div>
+                        <div className="post-content">
+                            <div className="before-highlight" dangerouslySetInnerHTML={{ __html: this.state.outputContent }}></div>
+                        </div>
                         <header className="post-footer"></header>
                     </div>
                 </div>
