9a82af4a1bde90c8dc02d6be3987651ca46f29dc
zcs02468 <zcs02468@163.com>
2020-05-20 21:01:09 +0800

    修改组件样式文件

--- client/src/components/blogList/index.tsx
+++ client/src/components/blogList/index.tsx
@@ -24,6 +24,13 @@ function BlogList() {
                 </div>
                 <PostMeta createTime={1589978600512}/>
             </div>
+            <img src="https://www.luymm.com/usr/uploads/2020/04/2382868296.gif" alt="thank-you-emergency-services-workers-6753651837108755-law.gif"></img>
+            <div className="item-content">
+            前言由于国内的 app store有很多应用是没有的，所以现在大家一般都有一个美服的 apple id，具体的注册方法大家可以自行搜索，不是特别困难。不过有些软件是收费的，比如 shadowro...
+            </div>
+            <div className="item-button">
+                <button>-&nbsp;阅读全文&nbsp;-</button>
+            </div>
         </div>
     )
 }
